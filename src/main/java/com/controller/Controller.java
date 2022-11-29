e1c8266e8f348aef9028e1a549e382e20497280a
jaswanth-mavuru <jaswanthkumarmavuru@gmail.com>
2022-11-29 19:06:25 +0530

    FirstApp

---
+++ src/main/java/com/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping("/API")
+	public String display()
+	{
+		return "Hello world";
+	}
+}
